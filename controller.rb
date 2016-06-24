445f2f3f6f10be8b027987d9c6756a2ee80ec809
Dave Lin & Lloyd Clave <pair+dvdlin214.llclave@devbootcamp.com>
2016-06-24 12:23:02 -0400

    add view class and refactor

--- controller.rb
+++ controller.rb
@@ -6,6 +6,7 @@ class Controller
     @deck = Deck.new(file)
     @view = View.new
     @file = file
+
   end
 
   def play_game
@@ -13,17 +14,20 @@ class Controller
     @deck.set_deck(@file)
     until i = @deck.cards.length
       playcard = @deck.select_card
-      puts playcard.question
-      answer = gets.chomp
-      point = @deck.verify(answer, playcard)
-      if point
-        puts "Correct!"
-      else
-        puts "Wrong!"
-      end
+      answer = card_question_prompt(playcard)
+      # puts playcard.question
+      # answer = gets.chomp
+      rightorwrong = @deck.verify(answer, playcard)
+      answer_verification(rightorwrong)
+      # if point
+      #   puts "Correct!"
+      # else
+      #   puts "Wrong!"
+      # end
       i += 1
     end
-    puts "You've answered #{@correct} of #{@deck.cards.length}"
+    total_correct(@deck.correct, @deck.cards.lenght)
+    # puts "You've answered #{@deck.correct} of #{@deck.cards.length}"
   end
 
 end
--- view.rb
+++ view.rb
@@ -1,4 +1,15 @@
 class View
+  def card_question_prompt(card)
+    puts "#{card.question}"
+    answer = gets.chomp
+  end
 
+  def answer_verification(rightorwrong)
+    rightorwrong ? puts "Correct!" : puts "Wrong!"
+  end
+
+  def total_correct(correct, total_cards)
+    puts "You've answered #{correct} of #{total_cards}"
+  end
 
 end
